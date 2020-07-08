6fbdea2e3ce6fcad9bb1924726b84e8f6d4b4ca4
Patrick Easter <epatri6@gmail.com>
2020-07-08 16:44:39 -0400

    Update app.js
    Updated store and outlined functions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Example store structure
  */
@@ -5,30 +7,64 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'What is an example of semantic HTML',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        '<div>',
+        '<img>',
+        '<span>',
+        '<footer>'
       ],
-      correctAnswer: 'green'
+      correctAnswer: '<footer>'
     },
     {
-      question: 'What is the current year?',
+      question: 'Which flexbox property spaces elements to opposite sides of the page?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'justify-content: space-evenly',
+        'justify-content: space-between',
+        'justify-content: center',
+        'justify-content: flex-start'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'justify-content: space-between'
+    },
+    {
+      question: 'What is the correct for loop structure to loop through all elements of an array?',
+      answers: [
+        'for(let i = 0; i <= array.length; i++)',
+        'for(let i = 1; i < array.length; i++)',
+        'for(let i = 0; i < array.length; i++)',
+        'for(let i; i <= array.length; i++)'
+      ],
+      correctAnswer: 'for(let i = 0; i < array.length; i++)'
+    },
+    {
+      question: 'What is a high order function?',
+      answers: [
+        'A function that returns a variable',
+        'A function that takes in a callback as an argument',
+        'An anonymous function',
+        'A function that returns an object'
+      ],
+      correctAnswer: 'A function that takes in a callback as an argument'
+    },
+    {
+      question: 'What is the correct jQuery selector to retrieve a main HTML tag',
+      answers: [
+        '$(\'main\')',
+        '$(main)',
+        '${\'main\'}',
+        '($\'main\')'
+      ],
+      correctAnswer: '$(\'main\')'
     }
   ],
   quizStarted: false,
   questionNumber: 0,
   score: 0
 };
+
+function main() {
+  generateView("start");
+}
 
 /**
  * 
@@ -49,10 +85,35 @@ const store = {
 
 // These functions return HTML templates
 
+function generateStartView() {
+  //return start view html
+}
+function generateQuestionView() {
+  //generate question html
+  //inject store data into question
+}
+function generateEndView() {
+  //return end view html
+}
+
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
+function render() {
+  //Based on id call view functions below
+  //Render html
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+function updateQuestionView() {}
+
+function startQuiz() {}
+
+function toggleAnswer() {}
+
+function submitAnswer() {}
+
+function nextQuestion() {}
