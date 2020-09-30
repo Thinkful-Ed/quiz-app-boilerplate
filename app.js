aa03458c561c1df61760ac1dee6e8fba8c301cac
Mayen Akpan <neroxma@yahoo.com>
2020-09-29 22:45:47 -0700

    Quiz

--- app.js
+++ app.js
@@ -1,58 +1,182 @@
-/**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
-  questions: [
+const STORE = {
+  questions: [//1
     {
-      question: 'What color is broccoli?',
+      question: 'Many businesses use which of the following to limit employees’ web access',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'Content filtering',
+        'Web bugs',
+        'Honeypots',
+        'Spyware'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'Content filtering'
     },
+    //2
     {
-      question: 'What is the current year?',
+      question: 'Which of the following allows access to high-speed Internet services through the cable television network?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Cable transceiver ',
+        'Cable modem ',
+        'Cable receiver ',
+        'Cable dialer'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'Cable modem '
+    },
+    //3
+    {
+      question: 'Which of the following terms is used to describe a program that hides in a computer and allows someone from a remote location to take full control of the computer? ',
+      answers: [
+        'A virus ',
+        'A worm ',
+        'A Trojan horse',
+        'A rootkit'
+      ],
+      correctAnswer: 'A rootkit'
+    },
+    //4
+    {
+      question: 'Which of the following kinds of programs displays an online advertisement in a banner or pop-up window on webpages, email, or other Internet service?',
+      answers: [
+        'Spyware', 
+        'Phishing', 
+        'Adware', 
+         'Trojan horse'
+         ],
+      correctAnswer: 'Adware'
+    },
+    //5
+    {
+      question: 'What is another term for data dictionary? ',
+      answers: [
+        'Index ',
+        'Repository ',
+        'Table',
+        'File'
+      ],
+      correctAnswer: 'Repository '
     }
   ],
-  quizStarted: false,
   questionNumber: 0,
   score: 0
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
+/*** Start Screen */
+function startScreenHtml(){
+  return `
+    <header>
+      <h1>Computer Science Quiz</h1>
+    </header>
+    <div class="startScreen">
+        <p>This quiz will assess your basic knowledge of Computer Science.</p>
+        <button type="button" id="start">Start Quiz</button>
+    </div>
+  `;
+}
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+function renderQuestion() {
+  let question = STORE.questions[STORE.questionNumber]
+  let questionHtml = currentQuestionHtml(question)
+  $('main').html(questionHtml)
+  $('form').submit(function(e) {
+    e.preventDefault()
 
-// These functions return HTML templates
+    let answer = $('input:radio[name=answer]:checked').val();
 
-/********** RENDER FUNCTION(S) **********/
+    // See if they actually picked one
+    if (answer === undefined) {
+      return; //If they didn't give an answer, dip out
+    }
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+    if (answer === question.correctAnswer) {
+      // Add +1 to the number correct
+      STORE.score++
+      
+      // Show the correct answer text
+      $('main').html(`
+        <h1>Answer page</h1>
+        <p>Correct </p>
+        <button class="next">Next</button>
+      `)
+    } else {
+      // Show the incorrect answer text
+      $('main').html(`
+        <h1>Answer page</h1>
+        <p>Wrong. The correct answer was ${question.correctAnswer} </p>
+        <button class="next">Next</button>
+      `)
+    }
+    STORE.questionNumber++
+  })
+}
 
-/********** EVENT HANDLER FUNCTIONS **********/
+function currentQuestionHtml(question){
+  let answer = question.answers
+  return `
+    <header>
+        <h1>Computer Science Quiz</h1>
+        <p>Current Question: ${STORE.questionNumber+1}</p>
+        <p>Score: ${STORE.score}</p>
+    </header>
+    <form>
+        <h3 class="Question"> ${question.question} </h3>
+        <label for="answer-0">
+            <input type="radio" id="answer-0" name="answer" value="${answer[0]}">${answer[0]}
+          </label>
+        <label for="answer-1">
+            <input type="radio" id="answer-1" name="answer" value="${answer[1]}">${answer[1]}
+          </label>
+        <label for="answer-2">
+            <input type="radio" id="answer-2" name="answer" value="${answer[2]}">${answer[2]}
+          </label>
+        <label for="answer-3">
+            <input type="radio" id="answer-3" name="answer" value="${answer[3]}">${answer[3]}
+          </label>
+        <button class="submitButton" type="submit">
+            Submit Question
+          </button>
+    </form>  
+  `
+}
 
-// These functions handle events (submit, click, etc)
+function next() {
+  $('main').on('click', '.next', function() {
+    let totalNumberOfQuestions = STORE.questions.length
+    let currentQuestion = STORE.questionNumber
+    if (currentQuestion >= totalNumberOfQuestions) {
+      $('main').html(restartScreenHtml())
+      $('#restart').click(function(e) {
+        resetScore();
+        renderStartScreen();
+      })
+    } else {
+      renderQuestion()
+    }
+  })
+}
+
+function renderStartScreen(){
+  $('main').html(startScreenHtml())
+  $('#start').click(function() {
+   renderQuestion()
+   next()
+  })
+}
+
+// HTML for results screen
+function restartScreenHtml(){
+  return`
+    <div class='quiz-result'>
+        <p>The Quiz is over.</p>
+        <p> You scored ${STORE.score}/${STORE.questions.length}</p>
+
+        <button type="button" id="restart"> Restart Quiz</button>
+    </div>
+  `;
+}
+
+function resetScore() {
+  STORE.score = 0;
+  STORE.questionNumber = 0;
+}
+
+$(renderStartScreen)
+  
